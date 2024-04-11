35a405b84852f06b538b797951e0e2c057c76673
amog1 <160067063+amog1@users.noreply.github.com>
2024-04-11 14:24:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// Average calculator
+
+// input
+let num1 = +prompt("Enter first number: ");
+let num2 = +prompt("Enter second number: ");
+let num3 = +prompt("Enter third number: ");
+//  process
+let average = (num1 + num2 + num3) / 3;
+
+// output
+alert("The average of the  number is : " + average);
